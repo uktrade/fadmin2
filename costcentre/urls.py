e844e1fea4e3a1c568ab58e177ba83c83f2789d0
Ross Miller <ross@decodedcollective.com>
2019-10-31 10:42:42 +0000

    Fix Flake8 issue

--- costcentre/urls.py
+++ costcentre/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from .views import FilteredCostHistoricalListView, FilteredCostListView
 
 urlpatterns = [
-    path("costcentrefilter/", FilteredCostListView.as_view(), name="cost_centre_filter"),
+    path(
+        "costcentrefilter/",
+        FilteredCostListView.as_view(),
+        name="cost_centre_filter",
+    ),
     path(
         "costcentrehistoricalfilter/",
         FilteredCostHistoricalListView.as_view(),
