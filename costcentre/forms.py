6c1716558b0c99b77766f85ca92ef96a5e5fb474
Ross Miller <ross@decodedcollective.com>
2019-11-06 17:39:11 +0000

    Add show/hide function to radio btns

--- costcentre/forms.py
+++ costcentre/forms.py
@@ -1,14 +1,61 @@
 from django import forms
 from django.forms import Select
 
+from guardian.shortcuts import get_objects_for_user
+
 from costcentre.models import CostCentre
 
 
-class ChooseCostCentreForm(forms.Form):
+class CostCentreViewModeForm(forms.Form):
+    COST_CENTRE_MODES = [
+        ('all', 'All cost centres'),
+        ('my', 'My cost centres'),
+    ]
+
+    mode = forms.ChoiceField(
+        choices=COST_CENTRE_MODES,
+        widget=forms.RadioSelect,
+    )
+
+    mode.widget.attrs.update(
+        {
+            #"class": "govuk-radios__input",
+            #"aria-describedby": "mode-hint mode-error",
+            'onclick': 'swapCostCentreChoice(this)',
+        }
+    )
+
+
+class AllCostCentresForm(forms.Form):
     cost_centre = forms.ModelChoiceField(
         queryset=CostCentre.objects.filter(
             active=True,
         ),
+        widget=Select(),
+    )
+
+    cost_centre.widget.attrs.update(
+        {
+            "class": "govuk-select",
+            "aria-describedby": "cost_centre-hint cost_centre-error",
+        }
+    )
+
+
+class MyCostCentresForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(MyCostCentresForm, self).__init__(
+            *args,
+            **kwargs,
+        )
+        self.base_fields['cost_centre'].queryset = get_objects_for_user(
+            user,
+            "costcentre.change_costcentre",
+        )
+
+    cost_centre = forms.ModelChoiceField(
+        queryset=None,
         widget=Select(),
     )
 
--- forecast/views/cost_centre_views.py
+++ forecast/views/cost_centre_views.py
@@ -1,16 +1,33 @@
 from django.urls import reverse
-from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 
-from costcentre.forms import ChooseCostCentreForm
+from guardian.shortcuts import get_objects_for_user
+
+from costcentre.forms import (
+    AllCostCentresForm,
+    CostCentreViewModeForm,
+    MyCostCentresForm,
+)
 
 
-class ChooseCostCentreView(FormView):
+class ChooseCostCentreView(TemplateView):
     template_name = "forecast/choose_cost_centre.html"
-    form_class = ChooseCostCentreForm
-    cost_centre_code = None
 
-    def get_success_url(self):
-        cost_centre_code = self.request.POST.get(
+    def cost_centre_mode_form(self):
+        return CostCentreViewModeForm()
+
+    def all_cost_centres_form(self):
+        return AllCostCentresForm()
+
+    def my_cost_centres_form(self):
+        return MyCostCentresForm(
+            user=self.request.user,
+        )
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        cost_centre_code = request.POST.get(
             'cost_centre',
             None,
         )
@@ -20,4 +37,4 @@ class ChooseCostCentreView(FormView):
                 kwargs={'cost_centre_code': cost_centre_code}
             )
 
-        return None
+        return self.render_to_response(context)
