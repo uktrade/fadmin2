d8222e6abcfb14842cf4b289b3b9f8481466a17d
Ross Miller <ross@decodedcollective.com>
2019-11-06 15:09:03 +0000

    Add document listing

--- upload_file/urls.py
+++ upload_file/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from upload_file.views import (
+    DocumentsView,
     SuccessfulUploadView,
+    UploadedView,
 )
 
 urlpatterns = [
-    path("upload-success/", SuccessfulUploadView.as_view(), name="upload_success"),
+    path("success/", SuccessfulUploadView.as_view(), name="upload_success"),
+    path("files/", UploadedView.as_view(), name="uploaded_files"),
+    path("documents/", DocumentsView.as_view(), name="upload_documents"),
 ]
--- upload_file/views.py
+++ upload_file/views.py
@@ -1,8 +1,37 @@
+from django.core.paginator import Paginator
+
 from django.views.generic.base import TemplateView
+
+from upload_file.models import FileUpload
+
+
+class UploadedView(TemplateView):
+    template_name = "upload_file/uploaded_files.html"
+
+    def uploaded_files(self):
+        uploaded_files = FileUpload.objects.all().order_by(
+            "-created"
+        )
+        paginator = Paginator(uploaded_files, 25)
+
+        page = self.request.GET.get('page')
+        return paginator.get_page(page)
 
 
 class SuccessfulUploadView(TemplateView):
     template_name = "core/info.html"
+
+    def heading(self):
+        return "Your upload was successful"
+
+    def message(self):
+        return "You have successfully " \
+               "uploaded a file, it will" \
+               " be processed shortly."
+
+
+class DocumentsView(TemplateView):
+    template_name = "upload_file/documents.html"
 
     def heading(self):
         return "Your upload was successful"
