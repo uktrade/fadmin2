17a6ed93560e9c5a69a6d994fa22befbe23c7c00
Ross Miller <ross@decodedcollective.com>
2019-11-11 16:41:31 +0000

    Fix unit tests broken by permission change

--- forecast/test/test_views.py
+++ forecast/test/test_views.py
@@ -21,7 +21,11 @@ from costcentre.test.factories import  (
 )
 
 from forecast.models import MonthlyFigure
-from forecast.views.edit_forecast import EditForecastView
+from forecast.views.edit_forecast import (
+    AddRowView,
+    ChooseCostCentreView,
+    EditForecastView,
+)
 
 
 # Nb. we're using RequestFactory here
@@ -114,35 +118,54 @@ class AddForecastRowTest(TestCase):
 
         self.test_user.set_password(self.test_password)
 
-    def test_view_add_row(self):
-        # Set up test objects
-        assign_perm("change_costcentre", self.test_user, self.cost_centre)
-        assign_perm("view_costcentre", self.test_user, self.cost_centre)
-
-        edit_forecast_url = reverse(
-            "edit_forecast",
-            kwargs={
-                'cost_centre_code': self.cost_centre_code
-            }
-        )
-
-        request = self.factory.get(edit_forecast_url)
+    def add_row_get_response(self, url):
+        request = self.factory.get(url)
         request.user = self.test_user
         kwargs = {
             'cost_centre_code': self.cost_centre_code
         }  # required because factory does not pass kwargs to request
-        edit_resp = EditForecastView.as_view()(request, **kwargs)
+        return AddRowView.as_view()(request, **kwargs)
 
-        self.assertEqual(edit_resp.status_code, 200)
+    def add_row_post_response(self, url, post_content):
+        request = self.factory.post(
+            url,
+            post_content,
+        )
+        request.user = self.test_user
+        kwargs = {
+            'cost_centre_code': self.cost_centre_code
+        }  # required because factory does not pass kwargs to request
+        return AddRowView.as_view()(request, **kwargs)
 
-        self.assertContains(edit_resp, "govuk-table")
-        soup = BeautifulSoup(edit_resp.content, features="html.parser")
+    def edit_row_get_response(self):
+        edit_view_url = reverse(
+            "edit_forecast",
+            kwargs={
+                'cost_centre_code': self.cost_centre_code
+            },
+        )
+        request = self.factory.get(edit_view_url)
+        request.user = self.test_user
+        kwargs = {
+            'cost_centre_code': self.cost_centre_code
+        }  # required because factory does not pass kwargs to request
+        return EditForecastView.as_view()(request, **kwargs)
+
+    def test_view_add_row(self):
+        # View edit, page, check there are 2 rows, view add page, add row, check there are 3 rows
+        assign_perm("change_costcentre", self.test_user, self.cost_centre)
+        assign_perm("view_costcentre", self.test_user, self.cost_centre)
+
+        edit_response = self.edit_row_get_response()
+        self.assertEqual(edit_response.status_code, 200)
+
+        soup = BeautifulSoup(edit_response.rendered_content, features="html.parser")
         table_rows = soup.find_all("tr", class_="govuk-table__row")
 
         # There should only be 2 rows (for the header and footer)
         assert len(table_rows) == 2
 
-        add_resp = self.client.get(
+        add_resp = self.add_row_get_response(
             reverse(
                 "add_forecast_row",
                 kwargs={
@@ -150,10 +173,11 @@ class AddForecastRowTest(TestCase):
                 },
             )
         )
+
         self.assertEqual(add_resp.status_code, 200)
 
         # add_forecast_row
-        add_row_resp = self.client.post(
+        add_row_resp = self.add_row_post_response(
             reverse(
                 "add_forecast_row",
                 kwargs={
@@ -163,14 +187,15 @@ class AddForecastRowTest(TestCase):
             {
                 "programme": self.programme.programme_code,
                 "natural_account_code": self.nac.natural_account_code,
-            },
-            follow=True,
+            }
         )
 
-        self.assertEqual(add_row_resp.status_code, 200)
+        self.assertEqual(add_row_resp.status_code, 302)
 
-        self.assertContains(add_row_resp, "govuk-table")
-        soup = BeautifulSoup(add_row_resp.content, features="html.parser")
+        edit_response = self.edit_row_get_response()
+        self.assertEqual(edit_response.status_code, 200)
+
+        soup = BeautifulSoup(edit_response.rendered_content, features="html.parser")
         table_rows = soup.find_all("tr", class_="govuk-table__row")
 
         # Now we should have 3 rows (header, footer and new row)
@@ -180,27 +205,8 @@ class AddForecastRowTest(TestCase):
         assign_perm("change_costcentre", self.test_user, self.cost_centre)
         assign_perm("view_costcentre", self.test_user, self.cost_centre)
 
-        # add_forecast_row
-        response = self.client.post(
-            reverse(
-                "add_forecast_row",
-                kwargs={
-                    'cost_centre_code': self.cost_centre_code
-                },
-            ), {
-                "programme": self.programme.programme_code,
-                "natural_account_code": self.nac.natural_account_code,
-                "analysis1_code": self.analysis_1_code,
-                "analysis2_code": self.analysis_2_code,
-                "project_code": self.project_code,
-            },
-            follow=True,
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(MonthlyFigure.objects.count(), 12)
-
-        response_2 = self.client.post(
+        # add forecast row
+        response = self.add_row_post_response(
             reverse(
                 "add_forecast_row",
                 kwargs={
@@ -213,69 +219,98 @@ class AddForecastRowTest(TestCase):
                 "analysis1_code": self.analysis_1_code,
                 "analysis2_code": self.analysis_2_code,
                 "project_code": self.project_code,
-            },
-            follow=True,
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(MonthlyFigure.objects.count(), 12)
+
+        response_2 = self.add_row_post_response(
+            reverse(
+                "add_forecast_row",
+                kwargs={
+                    'cost_centre_code': self.cost_centre_code
+                },
+            ),
+            {
+                "programme": self.programme.programme_code,
+                "natural_account_code": self.nac.natural_account_code,
+                "analysis1_code": self.analysis_1_code,
+                "analysis2_code": self.analysis_2_code,
+                "project_code": self.project_code,
+            }
         )
 
         self.assertEqual(response_2.status_code, 200)
 
         assert "govuk-list govuk-error-summary__list" in str(
-            response_2.content,
+            response_2.rendered_content,
         )
         self.assertEqual(MonthlyFigure.objects.count(), 12)
 
 
 class ChooseCostCentreTest(TestCase):
     def setUp(self):
+        self.factory = RequestFactory()
+
         self.cost_centre_code = 109076
         self.cost_centre = CostCentreFactory.create(
             cost_centre_code=self.cost_centre_code
         )
+        self.test_user, _ = get_user_model().objects.get_or_create(
+            email="test@test.com"
+        )
+        self.test_user.set_password("test_password")
 
-    def test_view_choose_cost_centre(self):
-        resp = self.client.get(
+        assign_perm("change_costcentre", self.test_user, self.cost_centre)
+        assign_perm("view_costcentre", self.test_user, self.cost_centre)
+
+    def factory_get(self, url):
+        request = self.factory.get(url)
+        request.user = self.test_user
+        kwargs = {
+            'cost_centre_code': self.cost_centre_code
+        }  # required because factory does not pass kwargs to request
+        return ChooseCostCentreView.as_view()(request, **kwargs)
+
+    def factory_post(self, url, post_content):
+        request = self.factory.post(
+            url,
+            post_content,
+        )
+        request.user = self.test_user
+        kwargs = {
+            'cost_centre_code': self.cost_centre_code
+        }  # required because factory does not pass kwargs to request
+        return ChooseCostCentreView.as_view()(request, **kwargs)
+
+    def test_choose_cost_centre(self):
+        response = self.factory_get(
             reverse(
                 "choose_cost_centre"
             )
         )
 
         self.assertEqual(
-            resp.status_code,
+            response.status_code,
             200,
         )
 
-        resp = self.client.post(
+        response = self.factory_post(
             reverse(
                 "choose_cost_centre"
             ), {
                 "cost_centre": self.cost_centre_code
             },
-            follow=True,
         )
 
         self.assertEqual(
-            resp.status_code,
-            200,
+            response.status_code,
+            302,
         )
 
-        # If the post has worked, we will have been
-        # redirect twice, first to the edit page and
-        # then to the cost centre page (because we don't
-        # have permission to view the edit page)
-        self.assertEqual(
-            len(resp.redirect_chain),
-            2
-        )
-        assert resp.redirect_chain[0] == (
-            '/forecast/edit/{}/'.format(
-                self.cost_centre_code,
-            ),
-            302,
-        )
-        assert resp.redirect_chain[1] == (
-            '/forecast/costcentre/',
-            302,
-        )
+        # Check we've been forwarded to edit page
+        assert "/forecast/edit/" in response.url
 
 
 class ViewCostCentreDashboard(TestCase):
