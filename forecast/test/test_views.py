510970b02adfd67332f807b7ccc16a88359d8b58
Luisella Strona <luisella21@hotmail.com>
2019-12-10 16:40:40 +0000

    Safety commit.

--- forecast/test/test_views.py
+++ forecast/test/test_views.py
@@ -382,6 +382,10 @@ class ViewForecastHierarchyTest(TestCase, RequestFactoryBase):
         ForecastPermissionFactory(
             user=self.test_user,
         )
+        self.year_total = self.amount_apr + self.amount_may
+        self.underspend_total = -self.amount_apr - self.amount_may
+        self.spend_to_date_total = self.amount_apr
+
 
     def test_dit_view(self):
         response = self.factory_get(
@@ -442,6 +446,63 @@ class ViewForecastHierarchyTest(TestCase, RequestFactoryBase):
         # Check directorate is shown
         assert str(self.cost_centre_code) in str(response.rendered_content)
 
+    def check_programme_table(self, table):
+        programme_rows = table.find_all("tr")
+        first_programme_cols = programme_rows[1].find_all("td")
+        assert first_programme_cols[1].get_text() == self.programme_obj.programme_description
+        assert first_programme_cols[2].get_text() == self.programme_obj.programme_code
+
+        last_programme_cols = programme_rows[-1].find_all("td")
+        # Check the total for the year
+        assert last_programme_cols[TOTAL_COLUMN].get_text() == intcomma(self.year_total)
+        # Check the difference between budget and year total
+        assert last_programme_cols[UNDERSPEND_COLUMN].get_text() == intcomma(self.underspend_total)
+        # Check the spend to date
+        assert last_programme_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(self.spend_to_date_total)
+
+    def check_expenditure_table(self, table):
+        expenditure_rows = table.find_all("tr")
+        first_expenditure_cols = expenditure_rows[1].find_all("td")
+        assert(first_expenditure_cols[1].get_text() == '—')
+
+        last_expenditure_cols = expenditure_rows[-1].find_all("td")
+        # Check the total for the year
+        assert last_expenditure_cols[TOTAL_COLUMN].get_text() == intcomma(self.year_total)
+        # Check the difference between budget and year total
+        assert last_expenditure_cols[UNDERSPEND_COLUMN].get_text() == intcomma(self.underspend_total)
+        # Check the spend to date
+        assert last_expenditure_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(self.spend_to_date_total)
+
+    def check_project_table(self, table):
+        project_rows = table.find_all("tr")
+        first_project_cols = project_rows[1].find_all("td")
+        assert first_project_cols[1].get_text() == self.project_obj.project_description
+        assert first_project_cols[2].get_text() == self.project_obj.project_code
+
+        last_project_cols = project_rows[-1].find_all("td")
+        # Check the total for the year
+        assert last_project_cols[TOTAL_COLUMN].get_text() == intcomma(self.year_total)
+        # Check the difference between budget and year total
+        assert last_project_cols[UNDERSPEND_COLUMN].get_text() == intcomma(self.underspend_total)
+        # Check the spend to date
+        assert last_project_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(self.spend_to_date_total)
+
+    def check_hierarchy_table(self, table, hierarchy_element):
+        hierarchy_rows = table.find_all("tr")
+        first_hierarchy_cols = hierarchy_rows[1].find_all("td")
+        assert (first_hierarchy_cols[1].get_text()) == hierarchy_element
+        # Check the April value
+        assert first_hierarchy_cols[4].get_text() == intcomma(self.amount_apr)
+
+        last_hierarchy_cols = hierarchy_rows[-1].find_all("td")
+        # Check the total for the year
+        assert last_hierarchy_cols[TOTAL_COLUMN].get_text() == intcomma(self.year_total)
+        # Check the difference between budget and year total
+        assert last_hierarchy_cols[UNDERSPEND_COLUMN].get_text() == intcomma(self.underspend_total)
+        # Check the spend to date
+        assert last_hierarchy_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(self.spend_to_date_total)
+
+
     def test_view_cost_centre_summary(self):
         resp = self.factory_get(
             reverse(
@@ -463,66 +524,91 @@ class ViewForecastHierarchyTest(TestCase, RequestFactoryBase):
         assert len(tables) == 4
 
         # Check that the first table displays the cost centre code
-        hierachy_rows = tables[HIERARCHY_TABLE_INDEX].find_all("tr")
-        first_hierarchy_cols = hierachy_rows[1].find_all("td")
-        assert int(first_hierarchy_cols[2].get_text()) == self.cost_centre_code
-        # Check the April value
-        assert first_hierarchy_cols[4].get_text() == intcomma(self.amount_apr)
-
-        last_hierarchy_cols = hierachy_rows[-1].find_all("td")
-        year_total = self.amount_apr + self.amount_may
-        underspend_total = -self.amount_apr - self.amount_may
-        spend_to_date_total = self.amount_apr
-        # Check the total for the year
-        assert last_hierarchy_cols[TOTAL_COLUMN].get_text() == intcomma(year_total)
-        # Check the difference between budget and year total
-        assert last_hierarchy_cols[UNDERSPEND_COLUMN].get_text() == intcomma(underspend_total)
-        # Check the spend to date
-        assert last_hierarchy_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(spend_to_date_total)
 
         # Check that all the subtotal hierachy_rows exist
         table_rows = soup.find_all("tr", class_="govuk-table__row")
         assert len(table_rows) == 18
 
-
+        self.check_hierarchy_table(tables[HIERARCHY_TABLE_INDEX], self.cost_centre.cost_centre_name)
         # Check that the second table displays the programme and the correct totals
-        programme_rows = tables[PROGRAMME_TABLE_INDEX].find_all("tr")
-        first_programme_cols = programme_rows[1].find_all("td")
-        assert first_programme_cols[1].get_text() == self.programme_obj.programme_description
-        assert first_programme_cols[2].get_text() == self.programme_obj.programme_code
-
-        last_programme_cols = programme_rows[-1].find_all("td")
-        # Check the total for the year
-        assert last_programme_cols[TOTAL_COLUMN].get_text() == intcomma(year_total)
-        # Check the difference between budget and year total
-        assert last_programme_cols[UNDERSPEND_COLUMN].get_text() == intcomma(underspend_total)
-        # Check the spend to date
-        assert last_programme_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(spend_to_date_total)
+        self.check_programme_table(tables[PROGRAMME_TABLE_INDEX])
 
         # Check that the third table displays the expenditure and the correct totals
-        expenditure_rows = tables[EXPENDITURE_TABLE_INDEX].find_all("tr")
-        first_expenditure_cols = expenditure_rows[1].find_all("td")
-        assert(first_expenditure_cols[1].get_text() == '—')
-
-        last_expenditure_cols = expenditure_rows[-1].find_all("td")
-        # Check the total for the year
-        assert last_expenditure_cols[TOTAL_COLUMN].get_text() == intcomma(year_total)
-        # Check the difference between budget and year total
-        assert last_expenditure_cols[UNDERSPEND_COLUMN].get_text() == intcomma(underspend_total)
-        # Check the spend to date
-        assert last_expenditure_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(spend_to_date_total)
+        self.check_expenditure_table(tables[EXPENDITURE_TABLE_INDEX])
 
         # Check that the second table displays the project and the correct totals
-        project_rows = tables[PROJECT_TABLE_INDEX].find_all("tr")
-        first_project_cols = project_rows[1].find_all("td")
-        assert first_project_cols[1].get_text() == self.project_obj.project_description
-        assert first_project_cols[2].get_text() == self.project_obj.project_code
+        self.check_project_table(tables[PROJECT_TABLE_INDEX])
 
-        last_project_cols = project_rows[-1].find_all("td")
-        # Check the total for the year
-        assert last_project_cols[TOTAL_COLUMN].get_text() == intcomma(year_total)
-        # Check the difference between budget and year total
-        assert last_project_cols[UNDERSPEND_COLUMN].get_text() == intcomma(underspend_total)
-        # Check the spend to date
-        assert last_project_cols[SPEND_TO_DATE_COLUMN].get_text() == intcomma(spend_to_date_total)
 
+    def test_view_directorate_summary(self):
+        resp = self.factory_get(
+            reverse(
+                "forecast_directorate",
+                kwargs={
+                    'directorate_code': self.directorate.directorate_code
+                },
+            ),
+            DirectorateView,
+            directorate_code=self.directorate.directorate_code,
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "govuk-table")
+        soup = BeautifulSoup(resp.content, features="html.parser")
+
+        # Check that there are 4 tables on the page
+        tables = soup.find_all("table", class_="govuk-table")
+        assert len(tables) == 4
+
+        # Check that the first table displays the cost centre code
+
+        # Check that all the subtotal hierachy_rows exist
+        table_rows = soup.find_all("tr", class_="govuk-table__row")
+        assert len(table_rows) == 18
+
+        self.check_hierarchy_table(tables[HIERARCHY_TABLE_INDEX], self.cost_centre.cost_centre_name)
+        # Check that the second table displays the programme and the correct totals
+        self.check_programme_table(tables[PROGRAMME_TABLE_INDEX])
+
+        # Check that the third table displays the expenditure and the correct totals
+        self.check_expenditure_table(tables[EXPENDITURE_TABLE_INDEX])
+
+        # Check that the second table displays the project and the correct totals
+        self.check_project_table(tables[PROJECT_TABLE_INDEX])
+
+
+    def test_view_group_summary(self):
+        response = self.factory_get(
+            reverse(
+                "forecast_group",
+                kwargs={
+                    'group_code': self.group.group_code
+                },
+            ),
+            GroupView,
+            group_code=self.group.group_code,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "govuk-table")
+        soup = BeautifulSoup(response.content, features="html.parser")
+
+        # Check that there are 4 tables on the page
+        tables = soup.find_all("table", class_="govuk-table")
+        assert len(tables) == 4
+
+        # Check that the first table displays the cost centre code
+
+        # Check that all the subtotal hierachy_rows exist
+        table_rows = soup.find_all("tr", class_="govuk-table__row")
+        assert len(table_rows) == 18
+
+        self.check_hierarchy_table(tables[HIERARCHY_TABLE_INDEX], self.directorate.directorate_name)
+        # Check that the second table displays the programme and the correct totals
+        self.check_programme_table(tables[PROGRAMME_TABLE_INDEX])
+
+        # Check that the third table displays the expenditure and the correct totals
+        self.check_expenditure_table(tables[EXPENDITURE_TABLE_INDEX])
+
+        # Check that the second table displays the project and the correct totals
+        self.check_project_table(tables[PROJECT_TABLE_INDEX])
