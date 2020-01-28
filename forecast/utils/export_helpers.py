22c0cdfc71b3d9b266fe5bfb5bd77df9ada13b2d
Luisella Strona <luisella21@hotmail.com>
2020-01-28 16:35:17 +0000

    More formatting.

--- forecast/utils/export_helpers.py
+++ forecast/utils/export_helpers.py
@@ -2,11 +2,34 @@ from django.http import HttpResponse
 
 import openpyxl
 
+from openpyxl import Workbook
+from openpyxl.utils import (
+    get_column_letter, 
+    column_index_from_string
+)
+
 from core.utils import today_string
 from core.exportutils import (
     EXC_TAB_NAME_LEN,
     EXCEL_TYPE,
 )
+
+from forecast.models import FinancialPeriod
+
+EDIT_BUDGET_COL = "F"
+EDIT_FIRST_MONTH_COL = "G"
+EDIT_LAST_MONTH_COL = "R"
+EDIT_YEAR_TO_DATE_COL = "S"
+EDIT_YEAR_TOTAL_COL = "T"
+EDIT_OVERSPEND_COL = "U"
+
+VIEW_BUDGET_COL = "X"
+VIEW_FIRST_MONTH_COL = "Y"
+VIEW_LAST_MONTH_COL = "AJ"
+VIEW_YEAR_TO_DATE_COL = "AK"
+VIEW_YEAR_TOTAL_COL = "AL"
+VIEW_OVERSPEND_COL = "AM"
+
 
 MONTH_HEADERS = [
     'Budget',
@@ -29,23 +52,6 @@ MONTH_HEADERS = [
 
 
 def forecast_query_iterator(queryset, columns_dict):
-    month_list = ['Budget',
-                  'Apr',
-                  'May',
-                  'Jun',
-                  'Jul',
-                  'Aug',
-                  'Sep',
-                  'Oct',
-                  'Nov',
-                  'Dec',
-                  'Jan',
-                  'Feb',
-                  'Mar']
-    l = list(columns_dict.values())
-    l.extend(month_list)
-    yield l
-
     for obj in queryset:
         row = []
         for field in columns_dict.keys():
@@ -69,23 +75,43 @@ def forecast_query_iterator(queryset, columns_dict):
         yield row
 
 
+def format_numbers(ws, row, start):
+    for c in range(start, start+16):
+        ws[f'{get_column_letter(c)}{row}'].number_format = '#,##0.00'
+
+
 def export_query_to_excel(queryset, columns_dict, title):
     resp = HttpResponse(content_type=EXCEL_TYPE)
     filename = f'{title}  {today_string()} .xlsx'
     resp["Content-Disposition"] = "attachment; filename=" + filename
-    wb = openpyxl.Workbook()
+    wb = Workbook()
     ws = wb.get_active_sheet()
     # Truncate the tab name to the maximum lenght permitted by Excel
     ws.title = f'{title} {today_string()}'[:EXC_TAB_NAME_LEN]
+    ws.append(create_headers(columns_dict, {}))
+
+    row_count = 1
+    first_actual = VIEW_FIRST_MONTH_COL
+    last_actual = last_actual_cell(first_actual)
     for row in forecast_query_iterator(queryset, columns_dict):
         ws.append(row)
+        row_count += 1
+        ws[f'{VIEW_FIRST_MONTH_COL}{row_count}'].number_format = '#,##0.00'
+        ws[f'{VIEW_BUDGET_COL}{row_count}'].number_format = '#,##0.00'
+        if last_actual:
+            ws[f'{VIEW_YEAR_TO_DATE_COL}{row_count}'].value = \
+                f'=SUM({first_actual}{row_count}:{last_actual}{row_count})'
+        ws[f'{VIEW_YEAR_TOTAL_COL}{row_count}'].value = \
+            f'=SUM({VIEW_FIRST_MONTH_COL}{row_count}:{VIEW_LAST_MONTH_COL}{row_count})'
+        ws[f'{VIEW_OVERSPEND_COL}{row_count}'].value = \
+            f'=({VIEW_BUDGET_COL}{row_count}-{VIEW_YEAR_TOTAL_COL}{row_count})'
+        format_numbers (ws, row_count, column_index_from_string(VIEW_BUDGET_COL))
+
     wb.save(resp)
     return resp
 
 
 def edit_forecast_query_iterator(queryset, keys_dict, columns_dict):
-
-
     for obj in queryset:
         row = []
         for field in keys_dict.keys():
@@ -117,8 +143,6 @@ def edit_forecast_query_iterator(queryset, keys_dict, columns_dict):
         yield row
 
 
-APRIL_COL = "G"
-
 def create_headers(keys_dict, columns_dict):
     k = list(keys_dict.values())
     l = list(columns_dict.values())
@@ -126,8 +150,13 @@ def create_headers(keys_dict, columns_dict):
     k.extend(l)
     return k
 
+
 def last_actual_cell(col):
-    return 'K'
+    actual_month = FinancialPeriod.financial_period_info.actual_month()
+    if actual_month:
+         return(get_column_letter(column_index_from_string(col) + actual_month))
+    return 0
+
 
 def export_edit_to_excel(queryset, key_dict, columns_dict, title):
     resp = HttpResponse(content_type=EXCEL_TYPE)
@@ -138,17 +167,26 @@ def export_edit_to_excel(queryset, key_dict, columns_dict, title):
     # Truncate the tab name to the maximum lenght permitted by Excel
     ws.title = f'{title} {today_string()}'[:EXC_TAB_NAME_LEN]
     row_count = 1
-    first_actual = 'G'
-    last_actual = 'K'
+    first_actual = EDIT_FIRST_MONTH_COL
+    last_actual = last_actual_cell(first_actual)
 
     ws.append(create_headers(key_dict, columns_dict))
     for data_row in edit_forecast_query_iterator(queryset, key_dict, columns_dict):
         ws.append(data_row)
         row_count += 1
-
-        ws[f'S{row_count}'].value = f'=SUM({first_actual}{row_count}:{last_actual}{row_count})'
-        ws[f'T{row_count}'].value = f'=SUM(G{row_count}:R{row_count})'
-        ws[f'U{row_count}'].value = f'=(F{row_count}-T{row_count})'
+        # Formula for Year To Date. Don't use it if there are no actuals
+        if last_actual:
+            ws[f'{EDIT_YEAR_TO_DATE_COL}{row_count}'].value = \
+                f'=SUM({first_actual}{row_count}:{last_actual}{row_count})'
+        else:
+            ws[f'{EDIT_YEAR_TO_DATE_COL}{row_count}'].value = 0
+        # Formula for calculating the full year
+        ws[f'{EDIT_YEAR_TOTAL_COL}{row_count}'].value = \
+            f'=SUM({EDIT_FIRST_MONTH_COL}{row_count}:{EDIT_LAST_MONTH_COL}{row_count})'
+        # Formula for overspend/underspend
+        ws[f'{EDIT_OVERSPEND_COL}{row_count}'].value = \
+            f'=({EDIT_BUDGET_COL}{row_count}-{EDIT_YEAR_TOTAL_COL}{row_count})'
+        format_numbers (ws, row_count, column_index_from_string(EDIT_BUDGET_COL))
 
     wb.save(resp)
     return resp
