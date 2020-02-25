e41027211f24c3f32715de12e5b83e4ee39e27b5
Ross Miller <ross@decodedcollective.com>
2020-02-25 17:28:52 +0000

    Fix flake8

--- forecast/permission_shortcuts.py
+++ forecast/permission_shortcuts.py
@@ -1,4 +1,4 @@
-from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.admin.models import CHANGE, LogEntry
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
