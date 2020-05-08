92248301df41e3be2183c9209c2ef0821c918238
Luisella Strona <luisella21@hotmail.com>
2020-05-08 18:31:11 +0100

    Flake8

--- costcentre/test/test_views.py
+++ costcentre/test/test_views.py
@@ -1,24 +1,21 @@
 import unittest
 
-from django.test import Client
+from django.test import (
+    Client,
+    TestCase
+)
 from django.urls import reverse
 
 import pytest
 
-from django.test import (
-    TestCase,
-)
-from django.urls import reverse
-
-
 from core.test.test_base import RequestFactoryBase
 
-from costcentre.views import FilteredCostListView
 from costcentre.test.factories import (
     CostCentreFactory,
     DepartmentalGroupFactory,
     DirectorateFactory,
 )
+from costcentre.views import FilteredCostListView
 
 
 @pytest.mark.django_db
@@ -48,8 +45,7 @@ class ViewCostCentreTest(TestCase, RequestFactoryBase):
         self.cost_centre_code = 109076
 
         self.group = DepartmentalGroupFactory(
-            group_code=self.group_code,
-            group_name=self.group_name,
+            group_code=self.group_code, group_name=self.group_name,
         )
         self.directorate = DirectorateFactory(
             directorate_code=self.directorate_code,
@@ -57,32 +53,22 @@ class ViewCostCentreTest(TestCase, RequestFactoryBase):
             group=self.group,
         )
         self.cost_centre = CostCentreFactory(
-            directorate=self.directorate,
-            cost_centre_code=self.cost_centre_code,
+            directorate=self.directorate, cost_centre_code=self.cost_centre_code,
         )
 
     def test_costcentre_view(self):
-        url = reverse(
-            "cost_centre_filter",
-            ),
+        url = (reverse("cost_centre_filter",),)
 
-        response = self.factory_get(url,
-            FilteredCostListView,
-        )
+        response = self.factory_get(url, FilteredCostListView,)
         self.assertEqual(response.status_code, 200)
 
         # Check cost centre is shown
         assert str(self.cost_centre_code) in str(response.rendered_content)
 
-
     def test_costcentre_download(self):
-        url = reverse(
-            "cost_centre_filter",
-        )
+        url = reverse("cost_centre_filter",)
         # add the argument required for downloading to excel
         url = f"{url}?_export=xlsx"
         print(url)
-        response = self.factory_get(url,
-            FilteredCostListView,
-        )
+        response = self.factory_get(url, FilteredCostListView,)
         self.assertEqual(response.status_code, 200)
