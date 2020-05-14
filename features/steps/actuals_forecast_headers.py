7b1d9c7e35fd0b6263f78a719e87fb2bc9b2c0c5
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-05-14 13:39:34 +0100

    Finalised feature tests

--- features/steps/actuals_forecast_headers.py
+++ features/steps/actuals_forecast_headers.py
@@ -13,27 +13,15 @@ from features.environment import (
     create_test_user,
 )
 
-from chartofaccountDIT.models import ProgrammeCode
-
 from forecast.models import (
-    FinancialCode,
     FinancialPeriod,
-    ForecastMonthlyFigure,
 )
 
 
 @given(u'the user views the edit forecast page with six months of actuals')
 def step_impl(context):
-    # ForecastMonthlyFigure.objects.all().delete()
-    # FinancialCode.objects.all().delete()
-    # programme_list = ProgrammeCode.objects.all()
-    # monthly_amount = 0
-    # # actual = FinancialPeriod.objects.all()
-    # # actual.actual_loaded = False
-    # for programme_fk in programme_list:
-    #     monthly_amount += 10
 
-    for i in range(1, 6):
+    for i in range(1, 7):
         actual = FinancialPeriod.objects.get(financial_period_code=i)
         actual.actual_loaded = True
         actual.save()
@@ -49,41 +37,46 @@ def step_impl(context):
 def step_impl(context):
 
     WebDriverWait(context.browser, 500).until(
-        ec.presence_of_element_located((By.ID, "actuals-header"))
+        ec.presence_of_element_located((By.ID, "actuals_header"))
     )
 
 
 @then(u'there are six actuals columns')
 def step_impl(context):
-    actuals_colspan = context.browser.find_element_by_id(
-        "actuals-header"
-    ).get_attribute(
+    actuals_header = context.browser.find_element_by_id(
+        "actuals_header"
+    )
+
+    actuals_colspan = actuals_header.get_attribute(
         'colspan'
     )
 
-    assert actuals_colspan == 6
+    assert actuals_colspan == "6"
 
 
 @given(u'the user views the edit forecast page with three months of actuals')
 def step_impl(context):
-    create_test_user(context)
 
     for i in range(1, 4):
         actual = FinancialPeriod.objects.get(financial_period_code=i)
         actual.actual_loaded = True
         actual.save()
 
-        context.browser.get(
-            f'{context.base_url}/forecast/edit/{TEST_COST_CENTRE_CODE}/'
-        )
+    create_test_user(context)
+
+    context.browser.get(
+        f'{context.base_url}/forecast/edit/{TEST_COST_CENTRE_CODE}/'
+    )
 
 
 @then(u'there are three actuals columns')
 def step_impl(context):
-    actuals_colspan = context.browser.find_element_by_id(
-        "actuals-header"
-    ).get_attribute(
+    actuals_header = context.browser.find_element_by_id(
+        "actuals_header"
+    )
+
+    actuals_colspan = actuals_header.get_attribute(
         'colspan'
     )
 
-    assert actuals_colspan == 3
+    assert actuals_colspan == "3"
