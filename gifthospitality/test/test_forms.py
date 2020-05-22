78cf3dadd47ccdb62902e5460875147d0d75d89c
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-05-22 16:22:12 +0100

    Tests completed

--- gifthospitality/test/test_forms.py
+++ gifthospitality/test/test_forms.py
@@ -1,10 +1,5 @@
-from datetime import datetime
-
 from bs4 import BeautifulSoup
 
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
 from django.test import (
     TestCase,
 )
@@ -12,13 +7,10 @@ from django.urls import reverse, path
 
 from core.test.test_base import RequestFactoryBase
 
-from gifthospitality.models import GiftAndHospitality, GiftAndHospitalityClassification, GiftAndHospitalityCompany, GiftAndHospitalityCategory, Grade
+from gifthospitality.models import GiftAndHospitality, GiftAndHospitalityClassification,\
+    GiftAndHospitalityCompany, GiftAndHospitalityCategory, Grade, DepartmentalGroup
 
-from gifthospitality.forms import GiftAndHospitalityReceivedForm
-from gifthospitality.views import GiftHospitalityReceivedView
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+from gifthospitality.forms import GiftAndHospitalityReceivedForm, GiftAndHospitalityOfferedForm
 
 
 class GiftHospitalityReceivedFormTest(TestCase, RequestFactoryBase):
@@ -30,14 +22,25 @@ class GiftHospitalityReceivedFormTest(TestCase, RequestFactoryBase):
             password=self.test_password,
         )
 
-        classification = GiftAndHospitalityClassification(sequence_no=10, gif_hospitality_classification="Test Classification")
-        classification.save()
+        self.classification = GiftAndHospitalityClassification(sequence_no="10", gif_hospitality_classification="98", active=True)
+        self.classification.save()
 
-        category = GiftAndHospitalityCategory(sequence_no=10, gif_hospitality_category="Test Category")
-        category.save()
+        self.category = GiftAndHospitalityCategory(sequence_no="10", gif_hospitality_category="10", active=True)
+        self.category.save()
 
-        # action_taken = GiftAndHospitality(action_taken="Rejected")
-        # action_taken.save()
+        grade = Grade(grade="Test", gradedescription="Test Grade")
+        grade.save()
+
+        departmental_group = DepartmentalGroup(group_code="8888AA", group_name="8888AA")
+        departmental_group.save()
+
+        self.company = GiftAndHospitalityCompany(sequence_no="10", gif_hospitality_company="10", active=True)
+        self.company.save()
+
+        action_taken = GiftAndHospitality(action_taken="Action1", date_offered="2006-05-23", value="12",
+                                          entered_date_stamp="2020-05-22", category_id="1", classification_id="1", classification=self.classification,
+                                          category=self.category,)
+        action_taken.save()
 
     def test_gift_hospitality_receive_form(self):
         response = reverse("gifthospitality:gift-received",)
@@ -46,42 +49,85 @@ class GiftHospitalityReceivedFormTest(TestCase, RequestFactoryBase):
 
         self.assertEqual(response.status_code, 200)
 
-        classification_filter = GiftAndHospitalityClassification.objects.get(sequence_no=10)
-        # print(classification_filter)
+        grade_filter = Grade.objects.get(grade="Test").grade
 
-        category_filter = GiftAndHospitalityCategory.objects.get(sequence_no=10)
+        group_filter = DepartmentalGroup.objects.get(group_code="8888AA").group_code
 
-        grade_filter = Grade.objects.all()
-
-        print(grade_filter)
-
+        action_taken_filter = GiftAndHospitality.objects.get(action_taken="Action1").date_offered
 
         gift_hospitality_received_data = {
-            'classification': classification_filter,     # Required
-            'category': category_filter,       # Required
-                                          'date_offered': '22-03-2005',     # Required
-                                          'action_taken': 'Rejected', # Required
-                                          'venue': 'Normal Venue',  # Required
-                                          'reason': 'Recommended by FD',  # Required
-                                          'value': '12',  # Required
-                            'rep': 'Someone from DIT',  # Required
-                            'grade': 'EO',  # Required
-                            'group': 'Departmental Group 0',  # Required
-                            'company_rep': 'Someone from a company',  # Required
-                            'company': 'ADS',  # Required
-                                                            }
-
+            'classification': self.classification.pk,     # Required
+            'category': self.category.pk,       # Required
+            'date_offered_0': action_taken_filter.day,
+            'date_offered_1': action_taken_filter.month,
+            'date_offered_2': action_taken_filter.year,
+            'action_taken': 'Action1', # Required
+            'venue': 'Normal Venue',  # Required
+            'reason': 'Recommended by FD',  # Required
+            'value': '12',  # Required
+            'rep': 'Someone from DIT',  # Required
+            'grade': grade_filter,  # Required
+            'group': group_filter,  # Required
+            'company_rep': 'Someone from a company',  # Required
+            'company': self.company.pk,  # Required
+        }
 
         self.assertContains(response, "govuk-button")
-        soup = BeautifulSoup(response.content, features="html.parser")
-
-        save_button = soup.find("save", class_="govuk-button")
 
         gift_hospitality_received_form = GiftAndHospitalityReceivedForm(data=gift_hospitality_received_data)
-        # print (gift_hospitality_received_form)
+        gift_hospitality_received_form.save()
 
-        # self.assertTrue(gift_hospitality_received_form.is_valid())
+        assert gift_hospitality_received_form.is_valid()
 
-        # redirect = save_button.click()
-        #
-        # self.assertEqual(redirect.status_code, 302)
+        response = reverse("gifthospitality:gift-offered", )
+
+        response = self.client.get(response)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class GiftHospitalitySearchTest(TestCase, RequestFactoryBase):
+    def setUp(self):
+
+        RequestFactoryBase.__init__(self)
+        self.client.login(
+            username=self.test_user_email,
+            password=self.test_password,
+        )
+
+        self.classification = GiftAndHospitalityClassification(sequence_no="10", gif_hospitality_classification="98",
+                                                               active=True)
+        self.classification.save()
+
+        self.category = GiftAndHospitalityCategory(sequence_no="10", gif_hospitality_category="10", active=True)
+        self.category.save()
+
+        grade = Grade(grade="Test", gradedescription="Test Grade")
+        grade.save()
+
+        departmental_group = DepartmentalGroup(group_code="8888AA", group_name="8888AA")
+        departmental_group.save()
+
+        self.company = GiftAndHospitalityCompany(sequence_no="10", gif_hospitality_company="10", active=True)
+        self.company.save()
+
+        action_taken = GiftAndHospitality(action_taken="Action1", date_offered="2006-05-23",  value="12", category_id="2", classification_id="2",
+                                          entered_date_stamp="2020-05-22", entered_by=self.test_user)
+        action_taken.save()
+
+    def test_search_records(self):
+
+        self.test_user.is_superuser=True
+        self.test_user.save()
+
+        response = reverse("gifthospitality:gift-search", )
+
+        response = self.client.get(response)
+
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, features="html.parser")
+
+        record_id = soup.find_all('td')
+
+        self.assertTrue(record_id)
