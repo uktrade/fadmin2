0553a2e3fa1c5328028adba8424c825fcb53ba19
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-06-02 08:42:58 +0100

    Broken test

---
+++ features/steps/gift_hospitality_other_company.py
@@ -0,0 +1,65 @@
+from behave import (
+    given,
+    when,
+    then,
+)
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+from features.environment import (
+    create_test_user,
+)
+
+
+@given(u'the user selects Other from the Company Received field')
+def step_impl(context):
+
+    create_test_user(context)
+
+    context.browser.get(
+        f'{context.base_url}/gifthospitality/receive'
+    )
+
+
+@when(u'the user selects Other')
+def step_impl(context):
+
+    WebDriverWait(context.browser, 500).until(
+        ec.presence_of_element_located((By.ID, "id_company"))
+    )
+
+
+@then(u'a free text field for Other appears')
+def step_impl(context):
+    company_field = context.browser.find_element_by_id(
+        'id_company'
+    )
+
+    options_list = company_field.find_elements_by_xpath('//option[@selected="other"]')
+    for other_company in options_list:
+        print(other_company.text)
+        if other_company.id == 'Other':
+            other_company.click()
+            break
+        else:
+            print("cant find")
+
+    # WebDriverWait(context.browser, 500).until(
+    #     ec.presence_of_element_located((By.CSS_SELECTOR, ))
+    # )
+
+    print("Hello")
+    other_company_field = context.browser.find_element_by_id('id_company_other')
+    parent = other_company_field.find_element_by_xpath('..')
+    print(parent)
+    import time
+    print(parent.get_attribute('style'))
+    print("goodbye")
+
+    assert parent.get_attribute("style") == 'display: none;'
+
+    # assert other_company_field.is_displayed() == True
+
+    # assert 1==2
