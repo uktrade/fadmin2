8b7b90b1f10d2c3e9812162d69d7f8bba3a859a4
Luisella Strona <luisella21@hotmail.com>
2020-06-15 09:45:13 +0100

    Test for end of month.

--- end_of_month/test/test_end_of_month_process.py
+++ end_of_month/test/test_end_of_month_process.py
@@ -86,74 +86,69 @@ PROJECT_TABLE_INDEX = 3
 def format_forecast_figure(value):
     return f'{round(value):,d}'
 
-
-def monthly_figure_setup():
-    group_name_test = "Test Group"
-    group_code_test = "TestGG"
-    directorate_name_test = "Test Directorate"
-    directorate_code_test = "TestDD"
-    cost_centre_code_test = 109076
-
-    group = DepartmentalGroupFactory(
-        group_code=group_code_test,
-        group_name=group_name_test,
-    )
-    directorate = DirectorateFactory(
-        directorate_code=directorate_code_test,
-        directorate_name=directorate_name_test,
-        group=group,
-    )
-    cost_centre = CostCentreFactory(
-        directorate=directorate,
-        cost_centre_code=cost_centre_code_test,
-    )
-    current_year = get_current_financial_year()
-    amount_apr = -9876543
-    programme_obj = ProgrammeCodeFactory()
-    nac_obj = NaturalCodeFactory()
-    project_obj = ProjectCodeFactory()
-    year_obj = FinancialYear.objects.get(financial_year=current_year)
-
-    apr_period = FinancialPeriod.objects.get(financial_period_code=1)
-    apr_period.actual_loaded = True
-    apr_period.save()
-    financial_code_obj = FinancialCode.objects.create(
-        programme=programme_obj,
-        cost_centre=cost_centre,
-        natural_account_code=nac_obj,
-        project_code=project_obj
-    )
-    financial_code_obj.save
-    apr_figure = ForecastMonthlyFigure.objects.create(
+def monthly_figure_save(year_obj, financial_code_obj, period):
+    month_figure = ForecastMonthlyFigure.objects.create(
         financial_period=FinancialPeriod.objects.get(
-            financial_period_code=1
+            financial_period_code=period
         ),
         financial_code=financial_code_obj,
         financial_year=year_obj,
-        amount=amount_apr
+        amount=period*100000
     )
-    apr_figure.save
-    amount_may = 1234567
-    may_figure = ForecastMonthlyFigure.objects.create(
-        financial_period=FinancialPeriod.objects.get(
-            financial_period_code=2,
-        ),
-        amount=amount_may,
-        financial_code=financial_code_obj,
-        financial_year=year_obj
-    )
-    may_figure.save
-    amount_oct = 99999
-    oct_figure = ForecastMonthlyFigure.objects.create(
-        financial_period=FinancialPeriod.objects.get(
-            financial_period_code=7,
-        ),
-        amount=amount_oct,
-        financial_code=financial_code_obj,
-        financial_year=year_obj
-    )
-    oct_figure.save
-    create_budget(financial_code_obj, year_obj)
+    month_figure.save
+
+
+class Monthly_figure_setup():
+
+    def monthly_figure_save(self, period):
+        month_figure = ForecastMonthlyFigure.objects.create(
+            financial_period=FinancialPeriod.objects.get(
+                financial_period_code=period
+            ),
+            financial_code=self.financial_code_obj,
+            financial_year=self.year_obj,
+            amount=period * 100000
+        )
+        month_figure.save
+
+    def __init__(self):
+        group_name_test = "Test Group"
+        group_code_test = "TestGG"
+        directorate_name_test = "Test Directorate"
+        directorate_code_test = "TestDD"
+        cost_centre_code_test = 109076
+
+        group = DepartmentalGroupFactory(
+            group_code=group_code_test,
+            group_name=group_name_test,
+        )
+        directorate = DirectorateFactory(
+            directorate_code=directorate_code_test,
+            directorate_name=directorate_name_test,
+            group=group,
+        )
+        cost_centre = CostCentreFactory(
+            directorate=directorate,
+            cost_centre_code=cost_centre_code_test,
+        )
+        current_year = get_current_financial_year()
+        amount_apr = 100000
+        programme_obj = ProgrammeCodeFactory()
+        nac_obj = NaturalCodeFactory()
+        project_obj = ProjectCodeFactory()
+        self. year_obj = FinancialYear.objects.get(
+            financial_year=current_year
+        )
+
+        self.financial_code_obj = FinancialCode.objects.create(
+            programme=programme_obj,
+            cost_centre=cost_centre,
+            natural_account_code=nac_obj,
+            project_code=project_obj
+        )
+        self.financial_code_obj.save
+        for month in range(1,13):
+            self.monthly_figure_save(month)
 
 
 class EndOfMonthTest(TestCase, RequestFactoryBase):
@@ -166,55 +161,113 @@ class EndOfMonthTest(TestCase, RequestFactoryBase):
         )
         self.test_user.user_permissions.add(can_view_forecasts)
         self.test_user.save()
-        monthly_figure_setup()
+        Monthly_figure_setup()
 
-    def test_end_of_month_april(self):
+    def test_end_of_month_apr(self):
         end_of_month_info = EndOfMonthStatus.objects.get(
             archived_period__financial_period_code=1
         )
         count = ForecastMonthlyFigure.objects.all().count()
-        print(f"Before {count}")
+        self.assertEqual(count, 12)
         end_of_month_archive(end_of_month_info)
         count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
-
-        end_of_month_info1 = EndOfMonthStatus.objects.get(
-            archived_period__financial_period_code=2
-        )
-        end_of_month_archive(end_of_month_info1)
-        count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
+        self.assertEqual(count, 24)
 
     def test_end_of_month_may(self):
+        self.test_end_of_month_apr()
         end_of_month_info = EndOfMonthStatus.objects.get(
-            archived_period__financial_period_code=1
-        )
-        count = ForecastMonthlyFigure.objects.all().count()
-        print(f"Before {count}")
-        end_of_month_archive(end_of_month_info)
-        count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
-
-        end_of_month_info1 = EndOfMonthStatus.objects.get(
             archived_period__financial_period_code=2
         )
-        end_of_month_archive(end_of_month_info1)
-        count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
-
-    def test_end_of_month_june(self):
-        end_of_month_info = EndOfMonthStatus.objects.get(
-            archived_period__financial_period_code=1
-        )
-        count = ForecastMonthlyFigure.objects.all().count()
-        print(f"Before {count}")
         end_of_month_archive(end_of_month_info)
         count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
+        self.assertEqual(count, 35)
 
-        end_of_month_info1 = EndOfMonthStatus.objects.get(
-            archived_period__financial_period_code=2
+    def test_end_of_month_jun(self):
+        self.test_end_of_month_may()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=3
         )
-        end_of_month_archive(end_of_month_info1)
+        end_of_month_archive(end_of_month_info)
         count = ForecastMonthlyFigure.objects.all().count()
-        print(f"After {count}")
+        self.assertEqual(count, 45)
+
+    def test_end_of_month_jul(self):
+        self.test_end_of_month_jun()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=4
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 54)
+
+    def test_end_of_month_aug(self):
+        self.test_end_of_month_jul()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=5
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 62)
+
+    def test_end_of_month_sep(self):
+        self.test_end_of_month_aug()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=6
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 69)
+
+    def test_end_of_month_oct(self):
+        self.test_end_of_month_sep()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=7
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 75)
+
+    def test_end_of_month_nov(self):
+        self.test_end_of_month_oct()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=8
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 80)
+
+    def test_end_of_month_dec(self):
+        self.test_end_of_month_nov()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=9
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 84)
+
+    def test_end_of_month_jan(self):
+        self.test_end_of_month_dec()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=10
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 87)
+
+    def test_end_of_month_feb(self):
+        self.test_end_of_month_jan()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=11
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 89)
+
+    def test_end_of_month_mar(self):
+        self.test_end_of_month_feb()
+        end_of_month_info = EndOfMonthStatus.objects.get(
+            archived_period__financial_period_code=12
+        )
+        end_of_month_archive(end_of_month_info)
+        count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(count, 90)
