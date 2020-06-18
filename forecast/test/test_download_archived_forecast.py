d58735a72f5f54bf5f0cc73c7b433573bd442cc5
Luisella Strona <luisella21@hotmail.com>
2020-06-18 08:51:48 +0100

    Test download

--- forecast/test/test_download_archived_forecast.py
+++ forecast/test/test_download_archived_forecast.py
@@ -126,8 +126,7 @@ class DownloadForecastHierarchyTest(TestCase, RequestFactoryBase):
         self.dit_download(0)
 
 
-    def test_group_download(self):
-        test_period = 2
+    def group_download(self, test_period):
         response = self.factory_get(
             reverse(
                 "export_forecast_data_group",
@@ -140,18 +139,49 @@ class DownloadForecastHierarchyTest(TestCase, RequestFactoryBase):
             group_code=self.archive.group_code,
             period=test_period,
         )
-
         self.assertEqual(response.status_code, 200)
+        self.check_workbook(response.content, test_period)
 
-        file = io.BytesIO(response.content)
-        wb = load_workbook(filename=file)
-        ws = wb.active
-        # Check group
-        assert ws[GROUP_HEADING_CELL].value == "Group name"
-        assert ws[GROUP_CODE_CELL].value == self.archive.group_code
+    def test_group_download_apr(self):
+        self.group_download(1)
 
-    def test_directorate_download(self):
-        test_period = 3
+    def test_group_download_may(self):
+        self.group_download(2)
+
+    def test_group_download_jun(self):
+        self.group_download(3)
+
+    def test_group_download_jul(self):
+        self.group_download(4)
+
+    def test_group_download_aug(self):
+        self.group_download(5)
+
+    def test_group_download_sep(self):
+        self.group_download(6)
+
+    def test_group_download_oct(self):
+        self.group_download(7)
+
+    def test_group_download_nov(self):
+        self.group_download(8)
+
+    def test_group_download_dec(self):
+        self.group_download(9)
+
+    def test_group_download_jan(self):
+        self.group_download(10)
+
+    def test_group_download_feb(self):
+        self.group_download(11)
+
+    def test_group_download_mar(self):
+        self.group_download(12)
+
+    def test_group_download_current(self):
+        self.group_download(0)
+
+    def directorate_download(self, test_period):
         response = self.factory_get(
             reverse(
                 "export_forecast_data_directorate",
@@ -164,19 +194,49 @@ class DownloadForecastHierarchyTest(TestCase, RequestFactoryBase):
             directorate_code=self.archive.directorate_code,
             period=test_period,
         )
-
         self.assertEqual(response.status_code, 200)
+        self.check_workbook(response.content, test_period)
 
-        file = io.BytesIO(response.content)
-        wb = load_workbook(filename=file, read_only=True)
-        ws = wb.active
-        # Check group
-        assert ws[GROUP_HEADING_CELL].value == "Group name"
-        assert ws[GROUP_CODE_CELL].value == self.archive.group_code
+    def test_directorate_download_apr(self):
+        self.directorate_download(1)
 
+    def test_directorate_download_may(self):
+        self.directorate_download(2)
 
-    def test_cost_centre_download(self):
-        test_period = 4
+    def test_directorate_download_jun(self):
+        self.directorate_download(3)
+
+    def test_directorate_download_jul(self):
+        self.directorate_download(4)
+
+    def test_directorate_download_aug(self):
+        self.directorate_download(5)
+
+    def test_directorate_download_sep(self):
+        self.directorate_download(6)
+
+    def test_directorate_download_oct(self):
+        self.directorate_download(7)
+
+    def test_directorate_download_nov(self):
+        self.directorate_download(8)
+
+    def test_directorate_download_dec(self):
+        self.directorate_download(9)
+
+    def test_directorate_download_jan(self):
+        self.directorate_download(10)
+
+    def test_directorate_download_feb(self):
+        self.directorate_download(11)
+
+    def test_directorate_download_mar(self):
+        self.directorate_download(12)
+
+    def test_directorate_download_current(self):
+        self.directorate_download(0)
+
+    def cost_centre_download(self, test_period):
         response = self.factory_get(
             reverse(
                 "export_forecast_data_cost_centre",
@@ -190,11 +250,43 @@ class DownloadForecastHierarchyTest(TestCase, RequestFactoryBase):
             period=test_period,
         )
         self.assertEqual(response.status_code, 200)
+        self.check_workbook(response.content, test_period)
 
-        file = io.BytesIO(response.content)
-        wb = load_workbook(filename=file)
-        ws = wb.active
-        # Check group
-        assert ws[GROUP_HEADING_CELL].value == "Group name"
-        assert ws[GROUP_CODE_CELL].value == self.archive.group_code
+    def test_cost_centre_download_apr(self):
+        self.cost_centre_download(1)
 
+    def test_cost_centre_download_may(self):
+        self.cost_centre_download(2)
+
+    def test_cost_centre_download_jun(self):
+        self.cost_centre_download(3)
+
+    def test_cost_centre_download_jul(self):
+        self.cost_centre_download(4)
+
+    def test_cost_centre_download_aug(self):
+        self.cost_centre_download(5)
+
+    def test_cost_centre_download_sep(self):
+        self.cost_centre_download(6)
+
+    def test_cost_centre_download_oct(self):
+        self.cost_centre_download(7)
+
+    def test_cost_centre_download_nov(self):
+        self.cost_centre_download(8)
+
+    def test_cost_centre_download_dec(self):
+        self.cost_centre_download(9)
+
+    def test_cost_centre_download_jan(self):
+        self.cost_centre_download(10)
+
+    def test_cost_centre_download_feb(self):
+        self.cost_centre_download(11)
+
+    def test_cost_centre_download_mar(self):
+        self.cost_centre_download(12)
+
+    def test_cost_centre_download_current(self):
+        self.cost_centre_download(0)
