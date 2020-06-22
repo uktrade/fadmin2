b2b18209c5b8c6924c95a8875d61b78823db32c9
Luisella Strona <luisella21@hotmail.com>
2020-06-22 10:46:55 +0100

    Improved the test.

--- end_of_month/test/test_delete_archive.py
+++ end_of_month/test/test_delete_archive.py
@@ -7,6 +7,9 @@ from end_of_month.end_of_month_actions import (
     delete_last_end_of_month_archive,
     end_of_month_archive,
 )
+from end_of_month.models import (
+    EndOfMonthStatus,
+)
 from end_of_month.test.test_utils import (
     MonthlyFigureSetup,
 )
@@ -14,7 +17,6 @@ from end_of_month.test.test_utils import (
 from core.test.test_base import RequestFactoryBase
 
 from forecast.models import (
-
     ForecastMonthlyFigure,
 )
 
@@ -34,21 +36,25 @@ class DeleteEndOfMonthArchiveTest(TestCase, RequestFactoryBase):
             delete_end_of_month_archive(1)
 
     def test_delete_latest_period(self):
-        initial_count = ForecastMonthlyFigure.objects.all().count()
-        self.assertEqual(initial_count, 15)
+        initial_forecast_count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(initial_forecast_count, 15)
         end_of_month_archive(1)
-        count = ForecastMonthlyFigure.objects.all().count()
-        self.assertEqual(count, 30)
+        forecast_count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(forecast_count, 30)
         delete_last_end_of_month_archive()
-        count = ForecastMonthlyFigure.objects.all().count()
-        self.assertEqual(count, initial_count)
+        forecast_count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(forecast_count, initial_forecast_count)
 
     def test_delete_selected_period(self):
         end_of_month_archive(1)
-        initial_count = ForecastMonthlyFigure.objects.all().count()
+        initial_forecast_count = ForecastMonthlyFigure.objects.all().count()
         end_of_month_archive(2)
-        count = ForecastMonthlyFigure.objects.all().count()
-        self.assertNotEqual(count, initial_count)
+        end_of_month_obj = EndOfMonthStatus.objects.get(archived_period=2)
+        self.assertEqual(end_of_month_obj.archived, True)
+        forecast_count = ForecastMonthlyFigure.objects.all().count()
+        self.assertNotEqual(forecast_count, initial_forecast_count)
         delete_end_of_month_archive(2)
-        count = ForecastMonthlyFigure.objects.all().count()
-        self.assertEqual(count, initial_count)
+        forecast_count = ForecastMonthlyFigure.objects.all().count()
+        self.assertEqual(forecast_count, initial_forecast_count)
+        end_of_month_obj = EndOfMonthStatus.objects.get(archived_period=2)
+        self.assertEqual(end_of_month_obj.archived, False)
