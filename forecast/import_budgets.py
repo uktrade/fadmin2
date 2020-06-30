1ae0a6faa3e52172e29b12ef3da168055ed3e7d2
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-06-30 15:08:30 +0100

    - translated

--- forecast/import_budgets.py
+++ forecast/import_budgets.py
@@ -79,11 +79,11 @@ def copy_uploaded_budget(year, month_dict):
 def upload_budget_figures(budget_row, year_obj, financialcode_obj, month_dict):
     for month_idx, period_obj in month_dict.items():
         period_budget = budget_row[month_idx].value
-        # for i in range(period_budget):
-        #     if i == '-':
-        #         i == 0
-        #     else:
-        #         i == i
+        for i in range(period_budget):
+            if i == '-':
+                i == 0
+            else:
+                i == i
         if period_budget:
             (budget_obj, created,) = BudgetUploadMonthlyFigure.objects.get_or_create(
                 financial_year=year_obj,
