b60b4626d883ba093907f93832d077b3a03fbb6e
Ross Miller <github@decodedcollective.com>
2020-10-07 15:42:08 +0100

    Fix logic

--- data_lake/views.py
+++ data_lake/views.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.utils.decorators import decorator_from_middleware
 
 from rest_framework.response import Response
@@ -7,6 +9,8 @@ from .hawk import (
     HawkAuthentication,
     HawkResponseMiddleware,
 )
+
+from core.utils.generic_helpers import get_current_financial_year
 
 from end_of_month.models import forecast_budget_view_model
 
@@ -37,6 +41,25 @@ class ForecastViewSet(
         pass
 
 
+def forecast_query_iterator(queryset, keys_dict, period_list, forecast_period, year):
+    for obj in queryset:
+        row = []
+        for field in keys_dict.keys():
+            val = obj[field]
+            if val is None:
+                val = ""
+            row.append(val)
+        row.append(obj["Budget"])
+
+        for period in period_list:
+            row.append(obj[period])
+
+        row.append(forecast_period)
+        row.append(year)
+
+        yield row
+
+
 def haha(request):
     # get relevant financial periods
     actual_periods_qs = FinancialPeriod.objects.filter(
@@ -45,19 +68,81 @@ def haha(request):
 
     actual_periods = [0, ] + list(actual_periods_qs)
 
-    period_output = []
-
     logger.error(actual_periods)
 
-    for period in actual_periods:
-        fields = ForecastQueryFields(period)
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename=forecast.csv"
+    writer = csv.writer(response, csv.excel)
+    response.write(u"\ufeff".encode("utf8"))  # Excel needs UTF-8 to open the file
 
-        data_model = forecast_budget_view_model[period]
+    # Titles
+    writer.writerow([
+        "Group name",
+        "Group code",
+        "Directorate name",
+        "Directorate code",
+        "Cost Centre name",
+        "Cost Centre code",
+        "Budget grouping",
+        "Expenditure type"
+        "Expenditure type description",
+        "Budget Type",
+        "Budget category",
+        "Budget category",
+        "Budget/Forecast NAC",
+        "Budget/Forecast NAC description",
+        "PO/Actual NAC",
+        "Natural Account code description",
+        "NAC Expenditure type",
+        "Programme code",
+        "Programme code description",
+        "Contract code",
+        "Contract description",
+        "Market code",
+        "Market description",
+        "Project code",
+        "Project description",
+        "Budget",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Adj1",
+        "Adj2",
+        "Adj3",
+        "Period",
+        "Year",
+        # "Forecast outturn",
+        # "Variance -overspend/underspend",
+        # "Year to Date Actuals"
+    ])
+
+    period_list = FinancialPeriod.financial_period_info.period_display_list()
+    current_year = get_current_financial_year()
+
+    for forecast_period in actual_periods:
+        fields = ForecastQueryFields(forecast_period)
+
+        data_model = forecast_budget_view_model[forecast_period]
         period_query = data_model.view_data.raw_data_annotated(
             fields.VIEW_FORECAST_DOWNLOAD_COLUMNS,
         )
-        period_output.append(
-            period_query,
-        )
 
-    return HttpResponse(period_output)
+        for data_row in forecast_query_iterator(
+            period_query,
+            fields.VIEW_FORECAST_DOWNLOAD_COLUMNS,
+            period_list,
+            forecast_period,
+            current_year,
+        ):
+            writer.writerow(data_row)
+
+    return response
