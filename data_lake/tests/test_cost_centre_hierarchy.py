60a3841eb057acd8aafddfd932046b800102f2ca
Luisella Strona <luisella21@hotmail.com>
2021-01-04 12:28:04 +0000

    Cost centre hierarchy

---
+++ data_lake/tests/test_cost_centre_hierarchy.py
@@ -0,0 +1,41 @@
+from rest_framework.reverse import reverse
+
+from data_lake.tests.test_hawk import (
+    hawk_auth_sender,
+)
+
+from django.test import (
+    TestCase,
+    override_settings,
+)
+
+from rest_framework.test import APIClient
+
+from costcentre.test.factories import (ArchivedCostCentreFactory, CostCentreFactory)
+
+class HierarchyTests(TestCase):
+    @override_settings(
+        HAWK_INCOMING_ACCESS_KEY="some-id",
+        HAWK_INCOMING_SECRET_KEY="some-secret",
+    )
+
+    def setUp(self):
+        self.cost_centre = CostCentreFactory.create().cost_centre_code
+        self.archived_cost_centre = ArchivedCostCentreFactory.create().cost_centre_code
+
+    def test_hierarchy_data_returned_in_response(self):
+
+        test_url = 'http://testserver' + reverse('data_lake_hierachy')
+        sender = hawk_auth_sender(url=test_url)
+        response = APIClient().get(
+            test_url,
+            content_type='',
+            HTTP_AUTHORIZATION=sender.request_header,
+            HTTP_X_FORWARDED_FOR='1.2.3.4, 123.123.123.123',
+        )
+
+        assert response['Content-Type'] == 'text/csv'
+        rows = response.content.decode("utf-8").split("\n")
+
+        cols = rows[0].split(",")
+        assert len(cols) == 42
--- data_lake/urls.py
+++ data_lake/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
-from .views import (
+from data_lake.views.forecast import (
     ForecastViewSet,
 )
+
+from data_lake.views.cost_centre_hierarchy import (
+    HierarchyViewSet,
+)
+
 
 urlpatterns = [
     path(
         "forecast/",
         ForecastViewSet.as_view({'get': 'list'}),
         name="data_lake_forecast",
+    ),
+    path(
+        "hierarchy/",
+        HierarchyViewSet.as_view({'get': 'list'}),
+        name="data_lake_hierachy",
     ),
 ]
