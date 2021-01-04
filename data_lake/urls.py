6ab0202a07fa3c3cee327e07e6a01e8e98830cdc
Luisella Strona <luisella21@hotmail.com>
2021-01-04 16:44:26 +0000

    Added code for exporting Natural Code

--- data_lake/urls.py
+++ data_lake/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from data_lake.views.forecast import ForecastViewSet
 
 from data_lake.views.cost_centre_hierarchy import HierarchyViewSet
-
+from data_lake.views.natural_code import NaturalCodeViewSet
 
 urlpatterns = [
     path(
@@ -15,5 +15,10 @@ urlpatterns = [
         "hierarchy/",
         HierarchyViewSet.as_view({"get": "list"}),
         name="data_lake_hierachy",
+    ),
+    path(
+        "naturalcode/",
+        NaturalCodeViewSet.as_view({"get": "list"}),
+        name="data_lake_natural_code",
     ),
 ]
