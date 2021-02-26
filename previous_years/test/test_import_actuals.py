1c2c8bfdd9cb9173a17fd4995f27941f4f87e906
Luisella Strona <luisella21@hotmail.com>
2021-02-26 18:46:15 +0000

    Safety commit.

--- previous_years/test/test_import_actuals.py
+++ previous_years/test/test_import_actuals.py
@@ -13,7 +13,6 @@ from previous_years.test.test_utils import (
     PastYearForecastSetup,
 )
 
-# ADD MIGRATION FOR FIXING ADJ
 
 class ImportPastYearActualTest(PastYearForecastSetup):
     def setUp(self):
@@ -22,15 +21,19 @@ class ImportPastYearActualTest(PastYearForecastSetup):
 
     def test_import_adj_2(self):
         chart_of_account_line_correct = \
-            f'3000-30000-{self.cost_centre_code}-{self.valid_natural_account_code}-{self.programme_code}-00000-00000-0000-0000-0000'
+            f'3000-30000-{self.cost_centre_code}-{self.natural_account_code}-{self.programme_code}-00000-00000-0000-0000-0000'
 
         period_obj = FinancialPeriod.objects.get(
             period_calendar_code=13
+            
         )
         year_obj = FinancialYear.objects.get(
             financial_year=self.archived_year
         )
-
+        self.assertEqual(
+            ArchivedActualUploadMonthlyFigure.objects.all().count(),
+            0,
+        )
         save_trial_balance_row(
             chart_of_account_line_correct,
             self.test_amount,
@@ -39,4 +42,11 @@ class ImportPastYearActualTest(PastYearForecastSetup):
             self.check_financial_code,
             2,
             ArchivedActualUploadMonthlyFigure
+        )
+        # Check that there is a row in the temporary table
+
+
+        self.assertEqual(
+            ArchivedActualUploadMonthlyFigure.objects.all().count(),
+            1,
         )
