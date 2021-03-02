b48548f90d7c74a4f6b808148cadf89cb7c676ac
Luisella Strona <luisella21@hotmail.com>
2021-03-02 17:46:20 +0000

    Added tests.

--- forecast/management/commands/set_actual_period.py
+++ forecast/management/commands/set_actual_period.py
@@ -32,31 +32,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            month_code = options["month_code"]
-            if month_code > MAX_PERIOD_CODE or month_code < 0:
+            period_code = options["period"]
+            if period_code > MAX_PERIOD_CODE or period_code < 0:
                 self.stdout.write(
                     self.style.ERROR("Valid Period is between 1 and {MAX_PERIOD_CODE}.")
                 )
                 return
-            financial_period_obj = FinancialPeriod.objects.get(
-                period_calendar_code=month_code
-            )
-            financial_period_code = financial_period_obj.financial_period_code
-            month_name = financial_period_obj.period_long_name
+            month_name = FinancialPeriod.objects.get(
+                financial_period_code=period_code
+            ).period_long_name
+
             if options["clear"]:
                 FinancialPeriod.objects.filter(
-                    financial_period_code__gte=financial_period_code
+                    financial_period_code__gte=period_code
                 ).update(actual_loaded=False)
                 FinancialPeriod.objects.filter(
-                    financial_period_code__lt=financial_period_code
+                    financial_period_code__lt=period_code
                 ).update(actual_loaded=True)
                 msg = f"Actual flag cleared up to {month_name}"
             else:
                 FinancialPeriod.objects.filter(
-                    financial_period_code__lte=financial_period_code
+                    financial_period_code__lte=period_code
                 ).update(actual_loaded=True)
                 FinancialPeriod.objects.filter(
-                    financial_period_code__gt=financial_period_code
+                    financial_period_code__gt=period_code
                 ).update(actual_loaded=False)
 
                 msg = f"Actual flag set up to {month_name}"
--- forecast/test/test_actual_period_command.py
+++ forecast/test/test_actual_period_command.py
@@ -3,31 +3,32 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 
-from forecast.models import FinancialPeriod
+from forecast.models import (
+    MAX_PERIOD_CODE,
+    FinancialPeriod,
+)
 
 
-class GiftsHospitalityCommandsTest(TestCase):
+class SetActualPeriodCommandsTest(TestCase):
     def setUp(self):
         self.out = StringIO()
 
     def set_all_actual(self):
         FinancialPeriod.objects.all().update(actual_loaded=True)
+        self.assertEqual(FinancialPeriod.objects.filter(actual_loaded=False).count(), 0)
 
     def clear_all_actual(self):
         FinancialPeriod.objects.all().update(actual_loaded=False)
+        self.assertEqual(FinancialPeriod.objects.filter(actual_loaded=True).count(), 0)
 
     def test_clear_all_actuals(self):
         self.set_all_actual()
-        self.assertEqual (
-            FinancialPeriod.objects.filter(actual_loaded=False).count(),
-            15
-        )
-        call_command(
-            "set_actual_period", "--clear", month=4, stdout=self.out
-        )
-        self.assertEqual (
-            FinancialPeriod.objects.filter(actual_loaded=False).count(),
-            0
-        )
+        call_command("set_actual_period", "--clear", 1, stdout=self.out)
+        self.assertEqual(FinancialPeriod.objects.filter(actual_loaded=True).count(), 0)
 
-
+    def test_set_all_actuals(self):
+        self.clear_all_actual()
+        call_command("set_actual_period", MAX_PERIOD_CODE, stdout=self.out)
+        self.assertEqual(
+            FinancialPeriod.objects.filter(actual_loaded=True).count(), MAX_PERIOD_CODE
+        )
