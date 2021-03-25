d8d5b4a6fe3cd7581f23a1f34d05cdaff0d5927f
Luisella Strona <luisella21@hotmail.com>
2021-03-25 15:59:45 +0000

    Fist attempt to create models.

--- split_project/models.py
+++ split_project/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+from core.metamodels import BaseModel
+from core.models import FinancialYear
+
+from forecast.models import FinancialCode, FinancialPeriod
+
+from previous_years.models import ArchivedFinancialCode
+
+
+class ProjectSplitCoefficientAbstract(BaseModel):
+    financial_period = models.ForeignKey(
+        FinancialPeriod,
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)ss",
+    )
+    financial_code_from = models.ForeignKey(
+        FinancialCode,
+        on_delete=models.PROTECT,
+        related_name="from_%(app_label)s_%(class)ss",
+    )
+    financial_code_to = models.ForeignKey(
+        FinancialCode,
+        on_delete=models.PROTECT,
+        related_name="to_%(app_label)s_%(class)ss",
+    )
+    coefficient = models.DecimalField(max_digits=5, decimal_places=4)
+    class Meta:
+        abstract=True
+        unique_together = (
+            "financial_period",
+            "financial_code_from",
+            "financial_code_to"
+        )
+
+
+class ArchivedProjectSplitCoefficient(ProjectSplitCoefficientAbstract):
+    financial_year = models.ForeignKey(FinancialYear, on_delete=models.PROTECT,)
+    financial_period = models.ForeignKey(
+        FinancialPeriod,
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)ss",
+    )
+    financial_code_from = models.ForeignKey(
+        ArchivedFinancialCode,
+        on_delete=models.PROTECT,
+        related_name="from_%(app_label)s_%(class)ss",
+    )
+    financial_code_to = models.ForeignKey(
+        ArchivedFinancialCode,
+        on_delete=models.PROTECT,
+        related_name="to_%(app_label)s_%(class)ss",
+    )
+    class Meta:
+        unique_together = (
+            "financial_year",
+            "financial_period",
+            "financial_code_from",
+            "financial_code_to"
+        )
+
+
+class ProjectSplitCoefficient(ProjectSplitCoefficientAbstract):
+    pass
