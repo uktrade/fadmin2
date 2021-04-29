4957e5f39fe30fc3068ece1d7f5d456dfdc50d18
Luisella Strona <luisella21@hotmail.com>
2021-04-29 17:24:18 +0100

    Refactor to a class.

--- split_project/import_project_percentage.py
+++ split_project/import_project_percentage.py
@@ -2,13 +2,9 @@ from django.db import connection
 
 from core.import_csv import xslx_header_to_dict
 
-from end_of_month.models import (
-    EndOfMonthStatus,
-)
+from end_of_month.models import EndOfMonthStatus
 
-from forecast.models import (
-    ForecastMonthlyFigure,
-)
+from forecast.models import ForecastMonthlyFigure
 
 from split_project.models import (
     ProjectSplitCoefficient,
@@ -47,10 +43,166 @@ EXPECTED_PERCENTAGE_HEADERS = [
 ]
 
 
-def copy_uploaded_percentage(month_dict):
-    for month_info in month_dict.values():
-        if month_info[1]:
-            period_obj = month_info[0]
+def create_month_dict(header_dict):
+    # Not all months are available in the excel file
+    # And we are only able to change month that have not yet been archived.
+    # Also, we may have the case of a month header, and no data in the columns
+    month_dict = {}
+    month_data_found_dict = {}
+    for month in EndOfMonthStatus.objects.filter(archived=False):
+        month_name = month.archived_period.period_short_name.lower()
+        if month_name in header_dict:
+            month_dict[header_dict[month_name]] = month.archived_period
+            month_data_found_dict[header_dict[month_name]] = False
+    if month_dict:
+        return month_dict, month_data_found_dict
+    raise UploadFileFormatError("Error: no month data")
+
+
+# def upload_project_percentage(worksheet, header_dict, month_dict, file_upload):
+#     # Clear the table used to upload the percentages.
+#     # The percentages are uploaded to to a temporary storage, and copied
+#     # when the upload is completed successfully.
+#     # This means that we always have a full upload.
+#     # But we ignored previous periods
+#     UploadProjectSplitCoefficient.objects.all().delete()
+#     rows_to_process = worksheet.max_row + 1
+#     check_financial_code = CheckFinancialCode(file_upload)
+#     cc_index = header_dict[COST_CENTRE_CODE]
+#     nac_index = header_dict[NAC_CODE]
+#     prog_index = header_dict[PROGRAMME_CODE]
+#     a1_index = header_dict[ANALYSIS1_CODE]
+#     a2_index = header_dict[ANALYSIS2_CODE]
+#     proj_index = header_dict[PROJECT_CODE]
+#     row_number = 0
+#     # There is a terrible performance hit accessing the individual cells:
+#     # The cell is found starting from cell A0, and continuing until the
+#     # required cell is found
+#     # The rows in worksheet.rows are accessed sequentially, so there is no
+#     # performance problem.
+#     # A typical files took over 2 hours to read using the cell access method
+#     # and 10 minutes with the row access.
+#     for percentage_row in worksheet.rows:
+#         row_number += 1
+#         if row_number == 1:
+#             # There is no way to start reading rows from a specific place.
+#             # Ignore first row, the headers have been processed already
+#             continue
+#         if not row_number % 100:
+#             # Display the number of rows processed every 100 rows
+#             set_file_upload_feedback(
+#                 file_upload, f"Processing row {row_number} of {rows_to_process}."
+#             )
+#         cost_centre = percentage_row[cc_index].value
+#         if not cost_centre:
+#             # protection against empty rows
+#             break
+#         check_financial_code.validate(
+#             cost_centre,
+#             percentage_row[nac_index].value,
+#             percentage_row[prog_index].value,
+#             percentage_row[a1_index].value,
+#             percentage_row[a2_index].value,
+#             percentage_row[proj_index].value,
+#             row_number,
+#         )
+#         if not check_financial_code.error_found:
+#             try:
+#                 upload_project_percentage_row(
+#                     percentage_row,
+#                     check_financial_code.get_financial_code(),
+#                     check_financial_code.get_financial_code_no_project(),
+#                     month_dict,
+#                 )
+#             except UploadFileDataError as ex:
+#                 check_financial_code.record_error(row_number, str(ex))
+#
+#     final_status = FileUpload.PROCESSED
+#     if check_financial_code.error_found:
+#         final_status = FileUpload.PROCESSEDWITHERROR
+#     else:
+#         # No errors, but check that are data to be used
+#         data_found = False
+#         for month_info in month_dict.values():
+#             if month_info[1]:
+#                 data_found = True
+#                 break
+#         if not data_found:
+#             final_status = FileUpload.PROCESSEDWITHERROR
+#             set_file_upload_fatal_error(
+#                 file_upload, "Error: no data specified.", "Upload aborted: Data error.",
+#             )
+#         else:
+#             # so we can copy the figures
+#             # from the temporary table to the percentages
+#             copy_uploaded_percentage(month_dict)
+#             if check_financial_code.warning_found:
+#                 final_status = FileUpload.PROCESSEDWITHWARNING
+#         try:
+#             for month_info in month_dict.values():
+#                 if month_info[1] and month_info[0].actual_loaded:
+#                     handle_split_project(
+#                         month_info[0].financial_period_code, ForecastMonthlyFigure
+#                     )
+#         except UploadFileDataError as ex:
+#             final_status = FileUpload.PROCESSEDWITHERROR
+#             set_file_upload_fatal_error(
+#                 file_upload, f"Error: {ex}.", "Upload aborted: Data error.",
+#             )
+#
+#     set_file_upload_feedback(
+#         file_upload, f"Processed {rows_to_process} rows.", final_status
+#     )
+#
+#     return not check_financial_code.error_found
+
+
+class UploadProjectPercentages:
+    def __init__(self, worksheet, header_dict, file_upload):
+        self.worksheet = worksheet
+        self.cc_index = header_dict[COST_CENTRE_CODE]
+        self.nac_index = header_dict[NAC_CODE]
+        self.prog_index = header_dict[PROGRAMME_CODE]
+        self.a1_index = header_dict[ANALYSIS1_CODE]
+        self.a2_index = header_dict[ANALYSIS2_CODE]
+        self.proj_index = header_dict[PROJECT_CODE]
+        self.file_upload = file_upload
+        self.row_to_process = self.worksheet.max_row + 1
+        self.month_dict, self.month_data_found_dict = create_month_dict(header_dict)
+
+    def display_row_count(self, row_number):
+        if not row_number % 100:
+            # Display the number of rows processed every 100 rows
+            set_file_upload_feedback(
+                self.file_upload,
+                f"Processing row {row_number} " f"of {self.rows_to_process}.",
+            )
+
+    def set_final_status(self, check_financial_code):
+        final_status = FileUpload.PROCESSED
+        if check_financial_code.warning_found:
+            final_status = FileUpload.PROCESSEDWITHWARNING
+
+        if check_financial_code.error_found:
+            final_status = FileUpload.PROCESSEDWITHERROR
+        else:
+            # No errors, but check that are data to be used
+            for month_idx, month_info in self.month_data_found_dict.items():
+                if not month_info:
+                    del self.month_dict[month_idx]
+
+            if not self.month_dict:
+                final_status = FileUpload.PROCESSEDWITHERROR
+                set_file_upload_fatal_error(
+                    self.file_upload,
+                    "Error: no data specified.",
+                    "Upload aborted: Data error.",
+                )
+        return final_status
+
+    def copy_uploaded_percentage(self):
+
+        for period_obj in self.month_dict.values():
             # Now copy the newly uploaded budgets to the monthly figure table
             ProjectSplitCoefficient.objects.filter(
                 financial_period=period_obj,
@@ -74,156 +226,92 @@ def copy_uploaded_percentage(month_dict):
                 financial_period=period_obj,
             ).delete()
 
-
-def upload_project_percentage_row(
-    percentage_row, financialcode_obj_to, financialcode_obj_from, month_dict
-):
-    for month_idx, month_info in month_dict.items():
-        period_percentage = percentage_row[month_idx].value
-        if period_percentage is None:
-            continue
-        # We import from Excel, and the user may have entered spaces in an empty cell.
-        if type(period_percentage) == str:
-            period_percentage = period_percentage.strip()
-        if period_percentage == "-":
-            # we accept the '-' as it is a recognised value in Finance for 0
-            period_percentage = 0
-        try:
-            period_percentage = int(period_percentage * 10000)
-            # there is a numeric value for this month.
-            month_info[1] = True
-        except ValueError:
-            raise UploadFileDataError(
-                f"Non-numeric value in {percentage_row[month_idx].coordinate}:"
-                f"{period_percentage}"
-            )
-        if period_percentage:
-            (
-                percentage_obj,
-                created,
-            ) = UploadProjectSplitCoefficient.objects.get_or_create(
-                financial_period=month_info[0],
-                financial_code_from=financialcode_obj_from,
-                financial_code_to=financialcode_obj_to,
-            )
-            if created:
-                percentage_obj.split_coefficient = period_percentage
-                percentage_obj.save()
-            else:
-                raise UploadFileDataError("Duplicate row.")
-
-
-def create_month_dict(header_dict):
-    # Not all months are available in the excel file
-    # And we are only able to change month that have not yet been archived.
-    # Also, we may have the case of a month header, and no data in the columns
-    # The dictionary has this format {month_index : [period_obj, data_found]}
-    month_dict = {}
-    for month in EndOfMonthStatus.objects.filter(archived=False):
-        month_name = month.archived_period.period_short_name.lower()
-        if month_name in header_dict:
-            month_dict[header_dict[month_name]] = [month.archived_period, False]
-    if month_dict:
-        return month_dict
-    raise UploadFileFormatError("Error: no month data")
-
-
-def upload_project_percentage(worksheet, header_dict, month_dict, file_upload):
-    # Clear the table used to upload the percentages.
-    # The percentages are uploaded to to a temporary storage, and copied
-    # when the upload is completed successfully.
-    # This means that we always have a full upload.
-    # But we ignored previous periods
-    UploadProjectSplitCoefficient.objects.all().delete()
-    rows_to_process = worksheet.max_row + 1
-    check_financial_code = CheckFinancialCode(file_upload)
-    cc_index = header_dict[COST_CENTRE_CODE]
-    nac_index = header_dict[NAC_CODE]
-    prog_index = header_dict[PROGRAMME_CODE]
-    a1_index = header_dict[ANALYSIS1_CODE]
-    a2_index = header_dict[ANALYSIS2_CODE]
-    proj_index = header_dict[PROJECT_CODE]
-    row_number = 0
-    # There is a terrible performance hit accessing the individual cells:
-    # The cell is found starting from cell A0, and continuing until the
-    # required cell is found
-    # The rows in worksheet.rows are accessed sequentially, so there is no
-    # performance problem.
-    # A typical files took over 2 hours to read using the cell access method
-    # and 10 minutes with the row access.
-    for percentage_row in worksheet.rows:
-        row_number += 1
-        if row_number == 1:
-            # There is no way to start reading rows from a specific place.
-            # Ignore first row, the headers have been processed already
-            continue
-        if not row_number % 100:
-            # Display the number of rows processed every 100 rows
-            set_file_upload_feedback(
-                file_upload, f"Processing row {row_number} of {rows_to_process}."
-            )
-        cost_centre = percentage_row[cc_index].value
-        if not cost_centre:
-            # protection against empty rows
-            break
-        check_financial_code.validate(
-            cost_centre,
-            percentage_row[nac_index].value,
-            percentage_row[prog_index].value,
-            percentage_row[a1_index].value,
-            percentage_row[a2_index].value,
-            percentage_row[proj_index].value,
-            row_number,
-        )
-        if not check_financial_code.error_found:
+    def upload_project_percentage_row(self, percentage_row):
+        for month_idx, month_obj in self.month_dict.items():
+            period_percentage = percentage_row[month_idx].value
+            if period_percentage is None:
+                continue
+            # We import from Excel, and the user
+            # may have entered spaces in an empty cell.
+            if type(period_percentage) == str:
+                period_percentage = period_percentage.strip()
+            if period_percentage == "-":
+                # we accept the '-' as it is a recognised value in Finance for 0
+                period_percentage = 0
             try:
-                upload_project_percentage_row(
-                    percentage_row,
-                    check_financial_code.get_financial_code(),
-                    check_financial_code.get_financial_code_no_project(),
-                    month_dict,
+                period_percentage = int(period_percentage * 10000)
+                # there is a numeric value for this month.
+                self.month_data_found_dict[month_idx] = True
+            except ValueError:
+                raise UploadFileDataError(
+                    f"Non-numeric value in {percentage_row[month_idx].coordinate}:"
+                    f"{period_percentage}"
                 )
-            except UploadFileDataError as ex:
-                check_financial_code.record_error(row_number, str(ex))
+            if period_percentage:
+                financialcode_obj_to = self.check_financial_code.get_financial_code()
+                financialcode_obj_from = (
+                    self.check_financial_code.get_financial_code_no_project()
+                )
+                (
+                    percentage_obj,
+                    created,
+                ) = UploadProjectSplitCoefficient.objects.get_or_create(
+                    financial_period=month_obj,
+                    financial_code_from=financialcode_obj_from,
+                    financial_code_to=financialcode_obj_to,
+                )
+                if created:
+                    percentage_obj.split_coefficient = period_percentage
+                    percentage_obj.save()
+                else:
+                    raise UploadFileDataError("Duplicate row.")
 
-    final_status = FileUpload.PROCESSED
-    if check_financial_code.error_found:
-        final_status = FileUpload.PROCESSEDWITHERROR
-    else:
-        # No errors, but check that are data to be used
-        data_found = False
-        for month_info in month_dict.values():
-            if month_info[1]:
-                data_found = True
+    def apply_percentages(self):
+        for month_obj in self.month_dict.values():
+            if month_obj.actual_loaded:
+                handle_split_project(
+                    month_obj.financial_period_code, ForecastMonthlyFigure
+                )
+
+    def read_percentages(self):
+        # Clear the table used to upload the percentages.
+        # The percentages are uploaded to to a temporary storage, and copied
+        # when the upload is completed successfully.
+        # This means that we always have a full upload.
+        # But we ignored previous periods
+        UploadProjectSplitCoefficient.objects.all().delete()
+        self.check_financial_code = CheckFinancialCode(self.file_upload)
+        row_number = 0
+        for percentage_row in self.worksheet.rows:
+            row_number += 1
+            if row_number == 1:
+                # There is no way to start reading rows from a specific place.
+                # Ignore first row, the headers have been processed already
+                continue
+            self.display_row_count(row_number)
+            cost_centre = percentage_row[self.cc_index].value
+            if not cost_centre:
+                # protection against empty rows
                 break
-        if not data_found:
-            final_status = FileUpload.PROCESSEDWITHERROR
-            set_file_upload_fatal_error(
-                file_upload, "Error: no data specified.", "Upload aborted: Data error.",
+            self.check_financial_code.validate(
+                cost_centre,
+                percentage_row[self.nac_index].value,
+                percentage_row[self.prog_index].value,
+                percentage_row[self.a1_index].value,
+                percentage_row[self.a2_index].value,
+                percentage_row[self.proj_index].value,
+                row_number,
             )
-        else:
-            # so we can copy the figures
-            # from the temporary table to the percentages
-            copy_uploaded_percentage(month_dict)
-            if check_financial_code.warning_found:
-                final_status = FileUpload.PROCESSEDWITHWARNING
-        try:
-            for month_info in month_dict.values():
-                if month_info[1] and month_info[0].actual_loaded:
-                    handle_split_project(
-                        month_info[0].financial_period_code, ForecastMonthlyFigure
-                    )
-        except UploadFileDataError as ex:
-            final_status = FileUpload.PROCESSEDWITHERROR
-            set_file_upload_fatal_error(
-                file_upload, f"Error: {ex}.", "Upload aborted: Data error.",
-            )
+            if not self.check_financial_code.error_found:
+                try:
+                    self.upload_project_percentage_row(percentage_row,)
+                except UploadFileDataError as ex:
+                    self.check_financial_code.record_error(row_number, str(ex))
 
-    set_file_upload_feedback(
-        file_upload, f"Processed {rows_to_process} rows.", final_status
-    )
-
-    return not check_financial_code.error_found
+        return (
+            self.set_final_status(self.check_financial_code)
+            != FileUpload.PROCESSEDWITHERROR
+        )
 
 
 def upload_project_percentage_from_file(file_upload):
@@ -243,17 +331,13 @@ def upload_project_percentage_from_file(file_upload):
         )
         workbook.close
         raise ex
-    try:
-        month_dict = create_month_dict(header_dict)
-    except UploadFileFormatError as ex:
-        set_file_upload_fatal_error(
-            file_upload, str(ex), str(ex),
-        )
-        workbook.close
-        raise ex
 
     try:
-        upload_project_percentage(worksheet, header_dict, month_dict, file_upload)
+        upload = UploadProjectPercentages(worksheet, header_dict, file_upload)
+        if upload.read_percentages():
+            upload.copy_uploaded_percentage()
+            upload.apply_percentages()
+
     except (UploadFileDataError) as ex:
         set_file_upload_fatal_error(
             file_upload, str(ex), str(ex),
